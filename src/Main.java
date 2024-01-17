a9c4cd53480e930656fccb3cc011da433550feea
Vlad Shuper <cristianbox777@gmail.com>
2024-01-17 12:51:28 +0300

    newimp

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        public static void main(String[] args) {
             int tiketPrice = 9000;
             int bonusMile = 20;
 
---
+++ src/OperatorBonus.java
@@ -0,0 +1,28 @@
+public class OperatorBonus {
+    public static void OperatorBonus (String[] args) {
+        int cashplus;
+        cashplus = 1200;
+        cashplus = 300;
+        int bonus;
+        if (cashplus > 1000) {
+            bonus = 1;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
+
+
+
+
+
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+    }
+}
