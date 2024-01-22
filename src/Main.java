15faab6e46abeeee68370d67fb812f09bb69c86f
Vlad Shuper <cristianbox777@gmail.com>
2024-01-22 13:20:30 +0300

    newbonusmile

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,8 @@
 public class Main {
-
     public static void main(String[] args) {
-            int tiketPrice = 9000;
-            int bonusMile = 20;
-
-            int bonus = tiketPrice / bonusMile;
-            System.out.println("Добрый день. Вам начисленны бонусные мили в размере:" + (bonus));
-
-        // Объявляете переменные для входных данных и
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной мили
-
-        // Рассчитываете количество бонусных миль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println("Добрый день. Вам начисленны бонусные мили в размере:" + (miles));
     }
 }
